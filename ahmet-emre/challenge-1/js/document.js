0cd894ef66887cc39591f71b19eef8cec5317b8c
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-07-31 21:50:53 +0300

    added ebruli

--- ahmet-emre/challenge-1/js/document.js
+++ ahmet-emre/challenge-1/js/document.js
@@ -1,19 +1,14 @@
 const ratingArea = document.querySelector(".rating-box-wrapper");
 const resultArea = document.querySelector(".rating-result-wrapper");
-
 const submitButton = document.querySelector(".rating-submit-input");
-
 const numberValue = document.querySelectorAll(".numbers");
-
 const outputText = document.querySelector(".result-text-display");
 let number = '';
 
 numberValue.forEach(function(e) {
   submitButton.setAttribute("disabled","true")
-
   e.addEventListener('focus',function() {
     numberValue.forEach((x) => x.classList.remove("selected"));
-
     e.classList.add('selected');
     submitButton.removeAttribute("disabled")
 
