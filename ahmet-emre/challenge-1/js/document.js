fb713b597ac3d515ca7e8a025314e01663951e0d
Emre <57330025+AEmre23@users.noreply.github.com>
2022-07-31 22:18:58 +0300

    fixed

--- ahmet-emre/challenge-1/js/document.js
+++ ahmet-emre/challenge-1/js/document.js
@@ -7,17 +7,12 @@ let number = '';
 
 numberValue.forEach(function(e) {
   submitButton.setAttribute("disabled","true")
-<<<<<<< HEAD
 
   e.addEventListener('click',function(eae) {
     eae.preventDefault();
-=======
-  e.addEventListener('focus',function() {
->>>>>>> 0cd894ef66887cc39591f71b19eef8cec5317b8c
     numberValue.forEach((x) => x.classList.remove("selected"));
     e.classList.add('selected');
     submitButton.removeAttribute("disabled")
-
     if (number==''){
     number += e.textContent;
     console.log(number);
@@ -26,7 +21,7 @@ numberValue.forEach(function(e) {
       number += e.textContent;
     }
   });
-})
+});
 
 submitButton.addEventListener('focus',function() {
   ratingArea.style.display = "none";
