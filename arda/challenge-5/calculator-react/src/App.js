554602a27edc7e27c9afd6fca395d3e3648ca168
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-09-08 20:58:30 +0300

    wtf

--- arda/challenge-5/calculator-react/src/App.js
+++ arda/challenge-5/calculator-react/src/App.js
@@ -4,13 +4,14 @@ import {useState} from "react";
 
 function App() {
     const [ result, setResult ] = useState(0);
+    const [ totalTip, setTotalTip ] = useState(0);
 
   return (
       <div className="bg-cyan-100 h-screen flex flex-col justify-center items-center">
           <h2 className="text-2xl text-gray-600">SPLITTER</h2>
           <div className="p-5 flex w-3/4 mx-auto bg-white rounded-xl">
-              <Calculate setResult={ setResult } />
-              <Result result ={ result } />
+              <Calculate setResult={ setResult } setTotalTip={setTotalTip} result={result} />
+              <Result result ={ result } totalTip = { totalTip } />
           </div>
       </div>
   );
--- arda/challenge-5/calculator-react/src/Component/Result.js
+++ arda/challenge-5/calculator-react/src/Component/Result.js
@@ -1,10 +1,10 @@
 import ResultInput from "./ResultInput";
 
-const Result = ({ result }) => {
+const Result = ({ result,totalTip }) => {
     return(
         <div className="bg-teal-900 w-1/2 p-10 rounded-2xl flex flex-col justify-between">
             <div>
-                <ResultInput content="Tip Amount" group="person" amount={result } />
+                <ResultInput content="Tip Amount" group="person" amount={totalTip} />
                 <ResultInput content="Total" group="person" amount={ result } />
             </div>
           <button className="bg-cyan-700 text-cyan-900 w-full p-2 rounded-md">RESET</button>
