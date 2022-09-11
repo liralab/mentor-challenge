3c20e197fbd0d0581ecf74d59782a09d7585f467
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-09-11 17:15:54 +0300

    all function works. :tada: @main

--- arda/challenge-5/calculator-react/src/Component/Calculate.js
+++ arda/challenge-5/calculator-react/src/Component/Calculate.js
@@ -3,30 +3,43 @@ import TipButton from "./TipButton";
 import {useEffect, useState} from "react";
 
 const values = [5,10,15,25,50, 'Custom'];
+
 const Calculate = ( { setResult, setTotalTip } ) => {
+    const [ totalTip, setTipTotal ] = useState(0);
     const [ bill, setBill ] = useState(0);
     const [ tip, setTip ] = useState(0 );
     const [ peopleCount, setPeopleCount ] = useState(0);
 
     useEffect(() => {
         calculateTip( tip, peopleCount, bill )
+        calculateBill( totalTip, bill )
         if( !bill ) {
-            setTotalTip(0)
+            setTotalTip(0 );
+            setResult( 0 );
         }
-    },[ tip, bill, peopleCount ])
+    },[ tip, bill, peopleCount, totalTip ])
 
     const calculateTip = ( tip, person, bill ) => {
         let newTip = Number(tip)
         let newPerson = Number( person )
-        let newBill = Number(bill)
+        let newBill = Number(bill )
 
         if(bill && !person ) {
             let tipRate = (newBill * newTip) / 100
             setTotalTip( tipRate )
         } else if( bill && person ) {
             let tipRate = (newBill * newTip) / 100
-            let totalTip = tipRate * newPerson
-            setTotalTip(totalTip)
+            setTipTotal( tipRate * newPerson )
+            setTotalTip( totalTip )
+        }
+    }
+
+    const calculateBill = ( totalTip, bill ) => {
+        const newBill = Number( bill );
+        if( bill ) {
+        setResult( (totalTip + newBill) * peopleCount );
+        } else {
+            setResult(0)
         }
     }
 
