993a9dfccaab288d6da1270ef5cb1f0b3eb8efc6
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-09-13 07:13:15 +0300

    calculate input converted tsx. migration going on :tada: @main

---
+++ arda/challenge-5/calculator-react/src/Component/CalculateInput.tsx
@@ -0,0 +1,28 @@
+type Props = {
+    value?: number,
+    label: string,
+    setValue: ( value: number ) => void,
+    result: number,
+    setResult: ( result: number ) => void,
+    name: string
+}
+
+const CalculateInput = ({ value, label, name, setValue, result, setResult } : Props ) => {
+
+    return(
+        <div className="flex flex-col mb-5">
+         <label className="text-zinc-600 text-md mb-2">{ label }</label>
+         <input
+             className="p-2 bg-cyan-100 rounded-xl"
+             value={ value }
+             name={ name }
+             id={ name }
+             onInput={ e => {
+             setValue( Number(( e.target as HTMLInputElement).value ))
+             result === 0 && setResult( Number(( e.target as HTMLInputElement).value ))
+         }}/>
+        </div>
+    )
+}
+
+export default CalculateInput;
